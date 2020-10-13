726738ba6a00ff4b9f0ff2b88fe00a90ba7715dc
ekf0120 <ekf0120@stu.kmu.ac.kr>
2020-10-14 04:30:15 +0900

    ver.1.5.2

---
+++ pos_System-master/Dashboard/app.js
@@ -0,0 +1,10 @@
+const orderLists = document.querySelector('[data-order-lists')
+
+function Item (name, num, price){
+    this.name = name
+    this.num = num
+    this.price = price
+}
+
+const addItemButton = document.querySelector('[pos_System_master="add-item"]')
+add
